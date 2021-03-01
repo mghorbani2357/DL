f3e54aa8bd9fdf1fbb3a6003b1afc060a7e1e946
Mohsen Ghorbani <m.ghorbani2357@gmail.com>
2021-03-01 23:51:55 +0330

    New threading

--- dl/Downloader.py
+++ dl/Downloader.py
@@ -1,8 +1,7 @@
 from urllib.request import urlopen, Request
-from hurry.filesize import size
-from threading import Thread
-from multiprocessing import Lock
 from multiprocessing.pool import ThreadPool
+from multiprocessing import Queue
+from multiprocessing import Lock
 
 
 class Downloader:
@@ -14,9 +13,9 @@ class Downloader:
     file_size = 0
     file_name = None
     download_file = None
+    lock = Lock()
 
     def __init__(self, threads=8, block_size=8196):
-        self.lock = Lock()
         self.thread_pool = ThreadPool(self.threads)
         self.threads = threads
         self.block_size = block_size
@@ -29,93 +28,34 @@ class Downloader:
         self.download_path = download_path
         self.file_name = url.split('/')[-1]
         self.file_size = int(meta.get("Content-Length"))
+
         self.download_file = open(self.download_path, 'wb')
         self.download_file.seek(self.file_size - 1)
         self.download_file.write(b'\0')
 
-        thread_download_size = int(self.file_size / self.threads)
+        queue = Queue()
 
-        threads = list()
-        for i in range(self.threads - 1):
-            threads.append(
-                Thread(target=self.download_thread, args=(i * thread_download_size, (i + 1) * thread_download_size)))
-        else:
-            i = self.threads.__len__()
+        for i in range(int(self.file_size / self.block_size)):
+            queue.put(i * self.block_size)
 
-        threads.append(Thread(target=self.download_thread, args=(i * thread_download_size, self.file_size)))
-
-        for thread in threads:
-            thread.start()
-
-        for thread in threads:
-            thread.join()
+        self.thread_pool.map(self.download_thread, (queue,))
 
         self.download_file.close()
 
-    def new_thread(self):
-        global i
-        while True:
-            self.lock.acquire()
-            if i >= 100:
-                break
-            i += 1
-            self.lock.release()
-        self.lock.release()
-
-    def download_thread(self, start, end):
-        print("I'm working")
-        request = Request(self.url)
-        request.add_header("Range", f"bytes={start}-{end}")
-        download_url = urlopen(request)
-
-        current_download_point = start
-        while current_download_point < end:
-
+    def download_thread(self, queue):
+        while not queue.empty():
+            pointer = queue.get()
+            request = Request(self.url)
+            request.add_header("Range", f"bytes={pointer}-{pointer + self.block_size}")
+            download_url = urlopen(request)
             buffer = download_url.read(self.block_size)
-            self.write_file(buffer, current_download_point)
-            current_download_point += len(buffer)
+            self.write_file(buffer, pointer)
+        print('done')
 
-            if not buffer:
-                break
-
-    def write_file(self, buffer, seek_address):
-        self.download_file.seek(seek_address)
+    def write_file(self, buffer, pointer):
+        self.lock.acquire()
+        self.download_file.seek(pointer)
         self.download_file.write(buffer)
         self.downloaded_size += len(buffer)
-
-        print(f"\r\033[kDownloading state: {size(self.downloaded_size)}  [%3.2f%%]" % (
-                self.downloaded_size * 100. / self.file_size), end='')
-
-
-class Queue:
-    jobs = list()
-
-    def __init__(self, jobs=[], max_workers=3, running=False):
-        self.jobs = jobs
-        self.running = running
-
-    def start(self):
-        self.running = True
-
-    def stop(self):
-        self.running = False
-
-    def worker(self):
-        while self.running:
-            running_threads = list()
-            for job in jobs:
-                while running_threads.__len__() <= self.max_workers:
-                    running_threads.append(Thread(target=job.task, args=job.args))
-                    running_threads[-1].start()
-
-
-class DownloadManager:
-    max_download_thread = 8
-
-    def __init__(self, max_download_thread=8):
-        self.max_download_thread = max_download_thread
-
-    def download(self, url, download_path):
-        downloader = Downloader(self.max_download_thread)
-
-        downloader.download(url, download_path)
+        print('\r', self.downloaded_size, end='')
+        self.lock.release()
--- main.py
+++ main.py
@@ -1,42 +1,102 @@
-# import argparse
-# from Downloader import Downloader
+# # import argparse
+# # from Downloader import Downloader
+# #
+# # # idm_url = "https://dl2.soft98.ir/soft/i/Internet.Download.Manager.6.38.Build.11.exe"
+# #
+# #
+# # parser = argparse.ArgumentParser(description='Downloader.')
+# #
+# # parser.add_argument('strings', metavar='L', type=str, nargs='+',
+# #                     help='an string for the download')
+# # parser.add_argument('--o', dest='output', help='output file path for download (default: file name from url)')
+# #
+# # if __name__ == '__main__':
+# #     args = parser.parse_args()
+# #     downloader = Downloader()
+# #     for url in args.strings:
+# #         downloader.download(url, args.output if args.output else url.split('/')[-1])
 #
-# # idm_url = "https://dl2.soft98.ir/soft/i/Internet.Download.Manager.6.38.Build.11.exe"
+#
+# # from multiprocessing import Lock
+# # from multiprocessing.pool import ThreadPool
+# # from multiprocessing.queues import Queue
+# # import multiprocessing as mp
+# #
+# # i = 0
+# #
+# #
+# # def func(q):
+# #     global i
+# #
+# #     while q.qsize() > 0:
+# #         q.get()
+# #         i += 1
+# #
+# #
+# # lock = Lock()
+# # ctx = mp.get_context('spawn')
+# # q = ctx.Queue()
+# # pool = ThreadPool(8)
+# #
+# # for i in range(30):
+# #     q.put(i)
+# #
+# # print(q.qsize())
+# #
+# # pool.map(func, (q,))
+# #
+# # print(q.qsize())
+# # print(i)
+#
+# #
+# # from multiprocessing import Queue
+# #
+# # colors = ['red', 'green', 'blue', 'black']
+# # cnt = 1
+# # # instantiating a queue object
+# # queue = Queue()
+# # print('pushing items to queue:')
+# # for color in colors:
+# #     print('item no: ', cnt, ' ', color)
+# #     queue.put(color)
+# #     cnt += 1
+# #
+# #
+# # def func(q):
+# #     cnt = 0
+# #     while not queue.empty():
+# #         print('item no: ', cnt, ' ', queue.get())
+# #         cnt += 1
+# #
+# # pool.map(func, (queue,))
+# #
+# # print(cnt)
+#
+# from multiprocessing.pool import ThreadPool
+# from multiprocessing import Queue
+# import time
+#
+# work = (["A", 5], ["B", 2], ["C", 1], ["D", 3])
 #
 #
-# parser = argparse.ArgumentParser(description='Downloader.')
+# def work_log(work_data):
+#     print(" Process %s waiting %s seconds" % (work_data[0], work_data[1]))
+#     time.sleep(int(work_data[1]))
+#     print(" Process %s Finished." % work_data[0])
 #
-# parser.add_argument('strings', metavar='L', type=str, nargs='+',
-#                     help='an string for the download')
-# parser.add_argument('--o', dest='output', help='output file path for download (default: file name from url)')
+#
+# def pool_handler():
+#     p = ThreadPool(4)
+#     p.map(work_log, work)
+#
 #
 # if __name__ == '__main__':
-#     args = parser.parse_args()
-#     downloader = Downloader()
-#     for url in args.strings:
-#         downloader.download(url, args.output if args.output else url.split('/')[-1])
+#     pool_handler()
 
 
-from multiprocessing import Lock
-from multiprocessing.pool import ThreadPool
+from dl.Downloader import Downloader
 
-i = 0
+downloader = Downloader()
 
-
-def func(var):
-    global i
-    while True:
-        lock.acquire()
-        if i >= 100:
-            break
-        i += 1
-        lock.release()
-    lock.release()
-
-
-lock = Lock()
-pool = ThreadPool(8)
-
-pool.map(func, range(8))
-
-print(i)
+downloader.download('file:///home/bluesp/Downloads/Soul.2020.720p.WEBRip.800MB.x264-GalaxyRG[TGx]/Soul.2020.720p.WEBRip.800MB.x264-GalaxyRG.mkv',
+                    'Soul.2020.720p.WEBRip.800MB.x264-GalaxyRG.mkv')
