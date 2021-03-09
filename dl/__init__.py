b569dd85b9cfd4d7519f06334bf014ce3930b4c9
Mohsen Ghorbani <m.ghorbani2357@gmail.com>
2021-03-09 22:24:53 +0330

    Download Manager added

--- dl/__init__.py
+++ dl/__init__.py
@@ -1,3 +1,4 @@
+import json
 from urllib.request import urlopen, Request
 from multiprocessing.pool import ThreadPool
 from multiprocessing import Lock
@@ -32,7 +33,7 @@ class Downloader:
                 download_path(str): Download path of file
                 threads(int): Threads count
                 block_size(int) : Download size in each block
-                limited_speed(int) : Speed limiter for download speed
+                limited_speed(int|float) : Speed limiter for download speed
         """
         self.url = url
         self.download_path = download_path
@@ -132,3 +133,23 @@ class Downloader:
         self.__lock.acquire()
         self.downloaded_size += len(buffer)
         self.__lock.release()
+
+
+class DownloadManager:
+    downloads = list()
+    parallel_downloads = 3
+    download_threads = 8
+    block_size = 8196
+    limit_speed = float('inf')
+
+    def __init__(self, json_db_path):
+        self.json_db_path = json_db_path
+        with open(self.json_db_path, 'r') as json_file:
+            self.json_db = json.load(json_file)
+
+    def save_state(self):
+        with open(self.json_db_path, 'w') as json_file:
+            json.dump(self.json_db, json_file)
+
+    def add_download(self, url, download_path, threads=8, block_size=8196, limited_speed=float('inf')):
+        self.downloads.append(Downloader(url, download_path, threads, block_size, limited_speed))
