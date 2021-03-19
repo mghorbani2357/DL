c2ec4be9520b083690d78ad46088003393c299dc
Mohsen Ghorbani <m.ghorbani2357@gmail.com>
2021-03-19 17:17:35 +0330

    Download-Manager updated

--- dl/manager.py
+++ dl/manager.py
@@ -11,20 +11,27 @@ class DownloadManager:
     counter = 0
 
     def __init__(self, json_db_path):
-        self.json_db_path = json_db_path
-        with open(self.json_db_path, 'r') as json_file:
-            self.json_db = json.load(json_file)
+        # self.json_db_path = json_db_path
+        # with open(self.json_db_path, 'r') as json_file:
+        #     self.json_db = json.load(json_file)
+        pass
 
-    def save_state(self):
-        with open(self.json_db_path, 'w') as json_file:
-            json.dump(self.json_db, json_file)
+    # def save_state(self):
+    #     with open(self.json_db_path, 'w') as json_file:
+    #         json.dump(self.json_db, json_file)
 
     def add_download(self, url, download_path, threads=8, block_size=8196, limited_speed=float('inf')):
         self.counter += 1
-        self.downloads[self.counter] = (Downloader(url, download_path, threads, block_size, limited_speed))
+        self.downloads[self.counter] = Downloader(url, download_path, threads, block_size, limited_speed)
 
     def start_download(self, download_id):
         self.downloads[download_id].download()
 
     def pause_download(self, download_id):
         self.downloads[download_id].pause()
+
+    def resume_download(self, download_id):
+        self.downloads[download_id].resume()
+
+    def delete_download(self, download_id):
+        self.downloads.pop(download_id)
